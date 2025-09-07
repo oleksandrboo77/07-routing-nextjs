c555ebfa67047a7042eca3926908fb959a360e08
oleksandrboo77 <oleksandrboo77@gmail.com>
2025-09-07 23:38:09 +0300

    add notepreview

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,13 +1,5 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import Modal from '@/components/Modal/Modal';
-import NoteDetailsClient from '@/app/notes/[id]/NoteDetails.client';
+import NotePreview from './NotePreview.client';
 
-export default function NotePreviewModal() {
-  const router = useRouter();
-  return (
-    <Modal onClose={() => router.back()}>
-      <NoteDetailsClient />
-    </Modal>
-  );
+export default function NotePreviewPage() {
+  return <NotePreview />;
 }
